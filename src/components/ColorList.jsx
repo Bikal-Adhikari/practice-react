99cd6ebdffa00c2e83dee791fb8aacd6ec98e4ac
Bikal <bikaladhikari5@gmail.com>
2024-05-02 19:34:26 +1000

    installing nanoid to generate id

--- src/components/ColorList.jsx
+++ src/components/ColorList.jsx
@@ -1,7 +1,12 @@
-import React from "react";
-
+import SingleColor from './SingleColor';
+import { nanoid } from 'nanoid';
 const ColorList = ({ colors }) => {
-  return <div>ColorList</div>;
+  return (
+    <section className='colors'>
+      {colors.map((color, index) => {
+        return <SingleColor key={nanoid()} color={color} index={index} />;
+      })}
+    </section>
+  );
 };
-
 export default ColorList;
