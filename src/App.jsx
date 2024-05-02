2dd9abce33cf51d844db12e0c325b7469b230c5f
Bikal <bikaladhikari5@gmail.com>
2024-05-02 19:31:50 +1000

    Install value.js to  use it to generate a list of colors in the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import ColorList from "./components/ColorList";
 import Form from "./components/Form";
+import Values from "values.js";
 
 const App = () => {
-  return <Form />;
+  const [colors, setColors] = useState(new Values("#f15025").all(10));
+  return (
+    <main>
+      <Form />
+      <ColorList colors={colors} />
+    </main>
+  );
 };
 export default App;
